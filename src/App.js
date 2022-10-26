6f81d439421b45b235f25482b5b10a399bc656e0
dineshpun <dineshpun96@gmail.com>
2022-10-26 14:37:42 +1030

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  // const title ='Welcome to the new blog';
+  // const likes =50;
+  // const link= "http://www.google.com";
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className='content'>
+        <Home/>
+        {/* <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+
+        <p> {10}</p>
+        <p>{"Hellow Woeld"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google link</a> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const Home = () => {
+    const [name, setName] = useState('Mario')
+    const [age, setAge] = useState(25);
+
+    const handleClick =(e)=>{
+        console.log('hello, ninjas',e);
+        setName('Luigi');
+        setAge('30');
+    }
+    const handleClickAgain=(name,e)=>{
+        console.log('Hello '+name, e.target);
+    }
+    return ( 
+        <div className="home">
+            <h2>Home Page</h2>
+            <p>{ name } is {age} years old.</p> <br></br>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e)=>handleClickAgain('Dinesh', e)}>Click me Again</button>
+        </div>
+     );
+}
+ 
+export default Home;
